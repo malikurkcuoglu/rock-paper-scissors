8aefedf4e7e3cfd7e957aa7ff02f5f6bf297f830
Mehmet Ali Kürkçüoğlu <115497918+malikurkcuoglu@users.noreply.github.com>
2025-05-20 03:27:42 +0300

    Add event listeners to buttons and remove playGame

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-let getComputerChoice = () => {
+const buttons = document.querySelectorAll(".buttons");
+const humanScoreDiv = document.querySelector(".human-score");
+const computerScoreDiv = document.querySelector(".computer-score");
+let humanChoice;
+let computerChoice;
+
+let humanScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    humanChoice = e.target.getAttribute("id");
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
+    console.log(humanChoice);
+  })
+);
+
+const getComputerChoice = () => {
   let randomNumber = Math.floor(Math.random() * 3);
   let computerChoice;
   switch (randomNumber) {
@@ -15,78 +34,45 @@ let getComputerChoice = () => {
   return computerChoice;
 };
 
-let getHumanChoice = () => {
-  let humanChoice = prompt("Enter your choice").toLowerCase();
-  return humanChoice;
-};
-
-let playGame = () => {
-  let humanScore = 0;
-  let computerScore = 0;
-  let isTie = false;
-  let playRound = (humanChoice, computerChoice) => {
-    if (humanChoice === "paper") {
-      if (computerChoice === "rock") {
-        isTie = false;
-        humanScore++;
-        console.log("You win! Paper beats rock");
-      } else if (computerChoice === "scissors") {
-        isTie = false;
-        computerScore++;
-        console.log("You lose! Scissors beats paper");
-      } else {
-        console.log("Tie");
-        isTie = true;
-      }
-    } else if (humanChoice === "rock") {
-      if (computerChoice === "scissors") {
-        isTie = false;
-        humanScore++;
-        console.log("You win! Rock beats scissors");
-      } else if (computerChoice === "paper") {
-        isTie = false;
-        computerScore++;
-        console.log("You lose! Paper beats rock");
-      } else {
-        isTie = true;
-        console.log("Tie");
-      }
+let playRound = (humanChoice, computerChoice) => {
+  if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      isTie = false;
+      humanScore++;
+      console.log("You win! Paper beats rock");
+    } else if (computerChoice === "scissors") {
+      isTie = false;
+      computerScore++;
+      console.log("You lose! Scissors beats paper");
     } else {
-      if (computerChoice === "paper") {
-        isTie = false;
-        humanScore++;
-        console.log("You win! Scissors beats paper");
-      } else if (computerChoice === "rock") {
-        isTie = false;
-        computerScore++;
-        console.log("You lose! Rock beats Scissors");
-      } else {
-        isTie = true;
-        console.log("Tie");
-      }
+      console.log("Tie");
+      isTie = true;
     }
-  };
-  let humanSelection;
-  let computerSelection;
-  let i = 1;
-  while (i <= 5) {
-    if (humanScore === 3 || computerScore === 3) break;
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    if (isTie) continue;
-    else i++;
-  }
-
-  if (humanScore > computerScore) {
-    console.log(
-      `You win! Your score : ${humanScore} Computer score : ${computerScore}`
-    );
+  } else if (humanChoice === "rock") {
+    if (computerChoice === "scissors") {
+      isTie = false;
+      humanScore++;
+      console.log("You win! Rock beats scissors");
+    } else if (computerChoice === "paper") {
+      isTie = false;
+      computerScore++;
+      console.log("You lose! Paper beats rock");
+    } else {
+      isTie = true;
+      console.log("Tie");
+    }
   } else {
-    console.log(
-      `You lose! Your score : ${humanScore} Computer score : ${computerScore}`
-    );
+    if (computerChoice === "paper") {
+      isTie = false;
+      humanScore++;
+      console.log("You win! Scissors beats paper");
+    } else if (computerChoice === "rock") {
+      isTie = false;
+      computerScore++;
+      console.log("You lose! Rock beats Scissors");
+    } else {
+      isTie = true;
+      console.log("Tie");
+    }
   }
 };
-
-playGame();
